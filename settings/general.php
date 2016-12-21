2b73724c6cd8593ce9a373365bb0f531a12b770f
David Cramer <david@digilab.co.za>
2016-12-21 13:12:52 +0200

    add location field slug

--- settings/general.php
+++ settings/general.php
@@ -14,23 +14,29 @@ $settings = array(
 	'label'       => __( 'General', 'facetwp' ),
 	'description' => __( 'General Setup for FacetWP Map', 'facetwp' ),
 	'control'     => array(
-		'api_key'       => array(
+		'api_key'        => array(
 			'label'       => __( 'API Key', 'facetwp' ),
 			'description' => __( 'Google Maps API Key.', 'facetwp' ),
 			'type'        => 'test',
 			'value'       => defined( 'GMAPS_API_KEY' ) ? GMAPS_API_KEY : '',
 		),
-		'group_markers' => array(
+		'group_markers'  => array(
 			'label'       => __( 'Group Markers', 'facetwp' ),
 			'description' => __( 'Enable Marker grouping when zoomed out.', 'facetwp' ),
 			'type'        => 'toggle',
 		),
-		'result_count'  => array(
+		'location_field' => array(
+			'label'       => __( 'Location Field', 'facetwp' ),
+			'description' => __( 'Custom Field slug that contains the location.', 'facetwp' ),
+			'value'       => 'location',
+			'type'        => 'text',
+		),
+		'result_count'   => array(
 			'label'       => __( 'Result Count', 'facetwp' ),
 			'description' => __( 'Set how many results are displayed on the map.', 'facetwp' ),
 			'type'        => 'select',
-			'choices' => array(
-				'all' => __( 'Display All results. No pagination', 'facetwp' ),
+			'choices'     => array(
+				'all'  => __( 'Display All results. No pagination', 'facetwp' ),
 				'page' => __( 'Adapt to templates pagination and results count.', 'facetwp' ),
 			),
 		),
