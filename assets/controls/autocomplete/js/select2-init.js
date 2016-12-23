e40cd03a6c1c9bb0f564dc9fa771c88688d20e36
David Cramer <david@digilab.co.za>
2016-12-23 07:40:52 +0200

    remove unused assets - fixes #6

--- assets/controls/autocomplete/js/select2-init.js
+++
@@ -1,7 +0,0 @@
-( function( $ ){
-    jQuery( function( $ ){
-        $( document ).on('facetwp_map.init', function(){
-            $('.facetwp_map-select2').select2();
-        });
-    });
-})( jQuery )
--- assets/controls/slider/js/ion.rangeSlider.init.js
+++
@@ -1,7 +0,0 @@
-( function( $ ){
-    jQuery( function( $ ){
-        $( document ).on('facetwp_map.init', function(){
-            $(".facetwp_map-slider").ionRangeSlider();
-        });
-    });
-})( jQuery )
