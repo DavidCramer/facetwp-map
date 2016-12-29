cd72af38024d61a291cc2d4dd1f22e07fc11bb0a
David Cramer <david@digilab.co.za>
2016-12-29 08:21:13 +0200

    grunt task for minimising assets

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        uglify: {
+            min: {
+                files: grunt.file.expandMapping( [
+                    'assets/js/*.js',
+                    '!assets/js/*.min.js',
+                    '!assets/js/*.min-latest.js'
+                ], 'assets/js/', {
+                    rename : function ( destBase, destPath ) {
+                        return destBase + destPath.replace( '.js', '.min.js' );
+                    },
+                    flatten: true
+                } )
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0
+            },
+            minify : {
+                expand: true,
+                cwd   : 'assets/css/',
+                src   : ['*.css', '!*.min.css'],
+                dest  : 'assets/css/',
+                ext   : '.min.css'
+            }
+        }
+    });
+
+    //load modules
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    //installer tasks
+    grunt.registerTask( 'default', [ 'cssmin', 'uglify' ] );
+
+};
