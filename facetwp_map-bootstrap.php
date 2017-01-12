6fa2c5ea7eb4ba3bcdf0113445c69e9041c20dd9
David Cramer <david@digilab.co.za>
2017-01-12 18:02:21 +0200

    cleanup duplicate constants.

--- facetwp_map-bootstrap.php
+++ facetwp_map-bootstrap.php
@@ -12,20 +12,11 @@
 // If this file is called directly, abort.
 if ( defined( 'WPINC' ) ) {
 
-    if ( ! defined( 'FWP_MAP_CORE' ) ) {
-        define( 'FWP_MAP_CORE', __FILE__ );
-        define( 'FWP_MAP_PATH', plugin_dir_path( __FILE__ ) );
-        define( 'FWP_MAP_URL', plugin_dir_url( __FILE__ ) );
-        define( 'FWP_MAP_VER', '1.0.1' );
+    if ( ! defined( 'DEBUG_SCRIPTS' ) ) {
+        define( 'FWP_MAP_ASSET_DEBUG', '.min' );
+    } else {
+        define( 'FWP_MAP_ASSET_DEBUG', '' );
     }
-    if ( ! defined( 'FWP_MAP_ASSET_DEBUG' ) ) {
-        if ( ! defined( 'DEBUG_SCRIPTS' ) ) {
-            define( 'FWP_MAP_ASSET_DEBUG', '.min' );
-        } else {
-            define( 'FWP_MAP_ASSET_DEBUG', '' );
-        }
-    }
-
 
     // include facetwp_map helper functions and autoloader.
     require_once( FWP_MAP_PATH . 'includes/functions.php' );
